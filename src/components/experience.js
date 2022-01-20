88a7981bc23e62528182c4f0aa6cb481401fb8b7
Muhammad Arslan <mughalarslan996@gmail.com>
2022-01-20 14:43:53 +0500

    Update experience.js

--- src/components/experience.js
+++ src/components/experience.js
@@ -23,21 +23,19 @@ export default function Experience() {
         <div className="text">
           <h4> Junior WEb Developer</h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit quibusdam eveniet dolor ipsam magnam.
+           Im working as a junior web developer.
           </p>
         </div>
       </div>
       <div className="box">
         <div className="year_company">
-          <h5>2021 - Present</h5>
-          <h5>Soft Shack</h5>
+          <h5>2017 - 2021</h5>
+          <h5>Cinepax</h5>
         </div>
         <div className="text">
-          <h4> Junior WEb Developer</h4>
+          <h4>Accountant</h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Reprehenderit quibusdam eveniet dolor ipsam magnam.
+           Im working at cinepax as an accountant from 2017 to 2021
           </p>
         </div>
       </div>
